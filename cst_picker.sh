ddb4da55e03f57302951a2519d10701740cd630e
Ahmed M. A. Elsherbini <49863685+AhmedElsherbini@users.noreply.github.com>
2022-07-30 18:23:10 +0200

    Add files via upload

---
+++ cst_picker.sh
@@ -0,0 +1,28 @@
+#cst_hunter
+echo I hope you activated you qiime2 enviroment
+#
+qiime taxa collapse \
+  --i-table feature-table.qza \
+  --i-taxonomy taxonomy.qza \
+  --p-level 7 \
+  --o-collapsed-table phyla-table.qza
+######################
+qiime feature-table relative-frequency \
+--i-table phyla-table.qza \
+--o-relative-frequency-table rel-phyla-table.qza
+#################
+qiime tools export \
+  --input-path rel-phyla-table.qza \
+  --output-path rel-table
+##################
+cd rel-table
+##################
+biom convert -i feature-table.biom -o rel-phyla-table.tsv --to-tsv
+#################
+awk '{a[NR]=$0}END{for(i=2;i<=NR;i++)print a[i]>FILENAME}' rel-phyla-table.tsv
+##################
+cp ../get_cst.py ./
+##################
+python3 get_cst.py -i rel-phyla-table.tsv
+##################
+echo Can you check the folder rel-table for output
---
+++ get_cst.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 28 00:42:02 2022
+
+@author: ahmed elsherbini
+"""
+import pandas as pd 
+import seaborn as sns_plot
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import  bar_label
+import argparse
+
+##################################################################
+my_parser = argparse.ArgumentParser(description='Welcome to our tool, make sure your files exsit in the data folder!')
+my_parser.add_argument('-i','--input',
+                       action='store',
+                       metavar='input',
+                       type=str,
+                       help='the path to your file')
+##################################################################
+args = my_parser.parse_args()
+f_name = args.input
+
+abundances = pd.read_csv(f_name, sep='\t',skiprows=0,index_col=0)
+
+mylist =  []
+##################################################################
+for i in abundances:
+     max_index = abundances[i].idxmax()
+     if "aureus" in max_index:
+         mylist.append([i,"CST1"])
+     elif "Enterobacteriaceae"  in max_index:
+          mylist.append([i,"CST2"])
+     elif "epidermidis"  in max_index:
+          mylist.append([i,"CST3"])
+     elif "Cutibacterium"  in max_index:
+          mylist.append([i,"CST4"])
+     elif "Corynebacterium"  in max_index:
+          mylist.append([i,"CST5"])
+     elif "Moraxella"  in max_index:
+          mylist.append([i,"CST6"])
+     elif "Dolosigranulum"  in max_index:
+          mylist.append([i,"CST_7"])
+     else:
+          mylist.append([i,"Unclassified"])
+##################################################################
+
+df = pd.DataFrame(mylist)
+
+df = df.rename(columns={'Community state type (CST)': 'Count'})
+df = df.rename(columns={0:"Sample_name", 1: "CST"})
+
+rel_df = pd.DataFrame(df["CST"].value_counts(normalize=True))
+
+rel_df.plot(kind = 'bar',legend=False,rot=0)
+#plt.xticks(rotation=30, horizontalalignment="center")
+plt.title("CST types")
+plt.ylabel("Relative abundace")
+
+df.to_csv('sample_cst_%s.csv'%(f_name), index=False)
+rel_df.to_csv('sample_cst_%s.csv'%(f_name), index=True)
+plt.savefig('cst_barplot_%s.png'%(f_name))   
